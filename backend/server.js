f73b741573eb6bfb0b3ce7f6c32a12228dac39ff
aaabdelgany <42511430+aaabdelgany@users.noreply.github.com>
2021-08-01 13:18:45 -0400

    production build

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,10 @@ app.use('/users', userRouter);
 app.use('/api/imgs', imageRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '/backend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'backend', 'build', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => {
