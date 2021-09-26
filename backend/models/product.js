118275b99d6ebf21ff015b3072ae209f5e8a4ba5
aaabdelgany <42511430+aaabdelgany@users.noreply.github.com>
2021-09-25 23:29:19 -0400

    Update product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -15,7 +15,7 @@ const productSchema = new Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
     name: { type: String, required: true },
-    image: { type: String, required: true },
+    image: { type: String },
     brand: { type: String, required: true },
     category: { type: String, required: true },
     description: { type: String },
